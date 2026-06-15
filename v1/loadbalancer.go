cd31f501f56003eea5e2134bbf1e3805c5c510dc
Karolin Kostial <KKostial@anexia.com>
2026-06-15 15:25:08 +0200

    implement get for load balancers

--- v1/loadbalancer.go
+++ v1/loadbalancer.go
@@ -2,6 +2,7 @@ package v1
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/anexia/go-anxsdk/internal"
 	"github.com/anexia/go-anxsdk/paging"
@@ -14,6 +15,23 @@ type LoadBalancerListParams struct {
 type LoadBalancerListItem struct {
 	Identifier string `json:"identifier"`
 	Name       string `json:"name"`
+}
+
+// GetID returns the Identifier of the LoadBalancerListItem.
+func (i LoadBalancerListItem) GetID() string {
+	return i.Identifier
+}
+
+type LoadBalancerGetResponse struct {
+	CustomerIdentifier         *string    `json:"customer_identifier"`
+	ResellerIdentifier         string     `json:"reseller_identifier"`
+	CriticalOperationPassword  *string    `json:"critical_operation_password"`
+	CriticalOperationConfirmed bool       `json:"critical_operation_confirmed"`
+	Identifier                 string     `json:"identifier"`
+	Name                       string     `json:"name"`
+	State                      State      `json:"state"`
+	IpAddress                  string     `json:"ip_address"`
+	AutomationRules            []Resource `json:"automation_rules"`
 }
 
 // LoadBalancerClient is an api client for managing load balancers.
@@ -40,4 +58,11 @@ func (c *LoadBalancerClient) ListPageFetcher(params LoadBalancerListParams) pagi
 	return func(ctx context.Context, pageParams paging.Params) (paging.PagedResponse[LoadBalancerListItem], error) {
 		return c.List(ctx, pageParams, params)
 	}
+}
+
+// Get returns a load balancer by identifier.
+func (c *LoadBalancerClient) Get(ctx context.Context, identifier string) (LoadBalancerGetResponse, error) {
+	resp := LoadBalancerGetResponse{}
+	err := c.transport.GetSingle(ctx, fmt.Sprintf("/api/LBaaS/v1/loadbalancer.json/%s", identifier), &resp)
+	return resp, mapTransportError(err)
 }
