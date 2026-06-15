9b598454e7f1b651b2387c92c4067168c0257d68
Karolin Kostial <KKostial@anexia.com>
2026-06-15 17:05:52 +0200

    implement backend get and list

---
+++ v1/backend.go
@@ -0,0 +1,73 @@
+package v1
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/anexia/go-anxsdk/internal"
+	"github.com/anexia/go-anxsdk/paging"
+)
+
+type BackendListParams struct {
+	Search string `url:"search,omitempty"`
+}
+
+type BackendListItem struct {
+	Identifier string `json:"identifier"`
+	Name       string `json:"name"`
+}
+
+// GetID returns the Itentifier of the BackendListItem.
+func (i BackendListItem) GetID() string {
+	return i.Identifier
+}
+
+type BackendGetResponse struct {
+	CustomerIdentifier         *string    `json:"customer_identifier"`
+	ResellerIdentifier         string     `json:"reseller_identifier"`
+	CriticalOperationPassword  *string    `json:"critical_operation_password"`
+	CriticalOperationConfirmed bool       `json:"critical_operation_confirmed"`
+	Identifier                 string     `json:"identifier"`
+	Name                       string     `json:"name"`
+	State                      State      `json:"state"`
+	Enable                     bool       `json:"enable"`
+	LoadBalancer               Resource   `json:"load_balancer"`
+	HealthCheck                string     `json:"health_check"`
+	Mode                       string     `json:"mode"`
+	ServerTimeout              int        `json:"server_timeout"`
+	Redeploy                   bool       `json:"redeploy"`
+	AutomationRules            []Resource `json:"automation_rules"`
+}
+
+// BackendClient is an api client for managing load balancer backends.
+type BackendClient struct {
+	transport *internal.Transport
+}
+
+// NewBackendClient creates a new backend client.
+func NewBackendClient(transport *internal.Transport) *BackendClient {
+	return &BackendClient{
+		transport: transport,
+	}
+}
+
+// List returns a list of paged backends.
+func (c *BackendClient) List(ctx context.Context, pagingParams paging.Params, params BackendListParams) (paging.PagedResponse[BackendListItem], error) {
+	resp := internal.RequestWrapper[paging.PagedResponse[BackendListItem]]{}
+	err := c.transport.Get(ctx, "/api/LBaaS/v1/backend.json", &resp, pagingParams, params)
+	return resp.Data, mapTransportError(err)
+}
+
+// ListPageFetcher returns a paging.PageFetcher for backends.
+func (c *BackendClient) ListPageFetcher(params BackendListParams) paging.PageFetcher[BackendListItem] {
+	return func(ctx context.Context, pageParams paging.Params) (paging.PagedResponse[BackendListItem], error) {
+		return c.List(ctx, pageParams, params)
+	}
+}
+
+// Get returns a backend by identifier.
+func (c *BackendClient) Get(ctx context.Context, identifier string) (BackendGetResponse, error) {
+	resp := BackendGetResponse{}
+	err := c.transport.GetSingle(ctx, fmt.Sprintf("/api/LBaaS/v1/backend.json/%s", identifier), &resp)
+	return resp, mapTransportError(err)
+}
